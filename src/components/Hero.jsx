208f17277a0a06fe670faee3508f9b9a1cbf6bef
techShabaka Agency <shahriarcode69@gmail.com>
2025-09-25 23:44:27 +0600

    navbar done

---
+++ src/components/Hero.jsx
@@ -0,0 +1,9 @@
+const Hero = () => {
+  return (
+    <section className="relative h-screen w-full bg-[url(/hero-bg.png)] bg-cover bg-center">
+      <h1>hello</h1>
+    </section>
+  );
+};
+
+export default Hero;
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,29 +12,35 @@ const Navbar = () => {
   ];
 
   return (
-    <nav className="fixed top-0 left-0 z-50 mx-auto w-full p-8">
-      <div className="liquid-glass box-shadow container flex justify-between px-5 py-4">
+    <nav className="fixed top-0 left-0 z-50 mx-auto h-auto w-full p-8">
+      <div className="liquid-glass container flex items-center justify-between px-5 py-3">
         <h1 className="text-2xl font-bold">MileStack.</h1>
-        <ul className="hidden gap-8 md:flex">
+        <ul className="hidden gap-12 md:flex">
           {navLinks.map((link) => (
-            <li key={link.name}>
+            <li key={link.id}>
               <a href={link.link}>{link.name}</a>
             </li>
           ))}
         </ul>
+        <div className="flex-center gap-4">
+          <button className="primary-btn box-shadow hidden md:block">
+            Sign Up
+          </button>
+        </div>
+
         <button className="md:hidden" onClick={() => setIsOpen(!isOpen)}>
           {isOpen ? <FaX size={24} /> : <FaBarsStaggered size={24} />}
         </button>
       </div>
 
       {isOpen && (
-        <div className="liquid-glass box-shadow mt-4 flex h-auto items-center justify-center py-12 md:hidden motion-translate-x-in-[167%] motion-blur-in-[21px] motion-duration-[0.4s]">
+        <div className="liquid-glass mt-4 flex flex-col gap-16 h-auto motion-blur-in-[21px] motion-translate-x-in-[167%] items-center justify-center py-12 motion-duration-[0.4s] md:hidden">
           <ul className="flex flex-col items-center gap-12 md:flex">
             {navLinks.map((link) => (
               <li key={link.name} className="">
                 <a
                   href={link.link}
-                  className="text-xl hover:cursor-pointer"
+                  className="text-base"
                   onClick={() => setIsOpen(false)}
                 >
                   {link.name}
@@ -42,6 +48,10 @@ const Navbar = () => {
               </li>
             ))}
           </ul>
+          <div className="h-0.5 w-52 bg-white/20"></div>
+          <button className="primary-btn box-shadow md:block">
+            Sign Up
+          </button>
         </div>
       )}
     </nav>

32482b3a3c75d9ffc577d305d9dcd588e1c6006e
techShabaka Agency <shahriarcode69@gmail.com>
2025-09-28 09:29:06 +0600

    add hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,10 +1,12 @@
 import { FaBoltLightning } from "react-icons/fa6";
+import { PiMouseSimple } from "react-icons/pi";
+import { MdKeyboardDoubleArrowDown } from "react-icons/md";
 
 const Hero = () => {
   return (
-    <section className="h-screen w-full bg-[url(/hero-bg.png)] bg-cover bg-fixed bg-center">
+    <section id="#" className="h-screen w-full bg-[url(/hero-bg.png)] bg-cover bg-fixed bg-center">
       <div className="relative container h-full pt-56">
-        <div className="absolute bottom-20">
+        <div className="absolute bottom-20 mx-8 2xl:mx-0 ">
           <h1 className="heading-xl">
             Manage Your Task <br /> Efficiently
           </h1>
@@ -13,7 +15,7 @@ const Hero = () => {
             sequi alias blanditiis?
           </p>
         </div>
-        <div className="backdrop-blur-base absolute top-45 right-0 inline-flex flex-col items-start justify-center gap-6 rounded-xl border border-white/30 bg-white/10 px-8 py-8">
+        <div className="backdrop-blur-base absolute top-45 right-0 inline-flex flex-col items-start justify-center gap-6 rounded-xl border border-white/30 bg-white/7 px-8 py-8 mx-8">
           <FaBoltLightning
             className="-mb-4 -rotate-20 text-amber-400"
             size={32}
@@ -26,11 +28,15 @@ const Hero = () => {
               Monthly Users <span className="font-bold">120k+</span>
             </p>
           </div>
-          <button>
-            <a href="#" className="primary-btn box-shadow mx-0">
+          <button className="flex w-full">
+            <a href="#" className="primary-btn box-shadow flex-1">
               Get Started
             </a>
           </button>
+        </div>
+        <div className=" hidden md:absolute bottom-20 right-0 md:flex flex-col gap-2 motion-preset-oscillate motion-duration-1000 mx-12">
+          <PiMouseSimple size={21}/>
+          <MdKeyboardDoubleArrowDown size={21}/>
         </div>
       </div>
     </section>

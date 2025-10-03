fc0d9cb4a40bb84b11631d8267fccf37d71e9472
techShabaka Agency <shahriarcode69@gmail.com>
2025-10-03 23:01:24 +0600

    update: hover animation added features bento

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -4,8 +4,8 @@ const Features = () => {
       <h2 className="heading-xl mb-12">Featues on the Table</h2>
 
       <div class="grid auto-rows-[85px] grid-cols-1 gap-2 md:grid-cols-6 lg:grid-cols-8">
-        <div class="relative col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-3 lg:col-span-3">
-          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-1.png)] bg-center bg-no-repeat md:left-44"></div>
+        <div class="group relative col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-3 lg:col-span-3">
+          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-1.png)] bg-center bg-no-repeat transition-all duration-700 ease-in-out group-hover:left-0 group-hover:group-hover:drop-shadow-[0_0_20px] group-hover:drop-shadow-blue-500 md:left-44"></div>
           <h3 class="subtitle z-10">Automate repetitive work.</h3>
         </div>
 
@@ -14,13 +14,13 @@ const Features = () => {
           <h3 class="subtitle z-20 text-center">End-to-End Encryption</h3>
         </div>
 
-        <div class="relative col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-2 md:row-span-5 lg:col-span-2">
-          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-2.png)] bg-center bg-no-repeat"></div>
+        <div class="relative group col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-2 md:row-span-5 lg:col-span-2">
+          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-2.png)] bg-center bg-no-repeat transition-all duration-700 ease-in-out group-hover:rotate-360 group-hover:group-hover:drop-shadow-[0_0_20px] group-hover:drop-shadow-blue-500"></div>
           <h3 class="subtitle z-10">AI helps users set priorities</h3>
         </div>
 
-        <div class="relative col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-3 md:row-span-3 lg:col-span-3">
-          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-3.png)] bg-center bg-no-repeat md:left-45"></div>
+        <div class="group relative col-span-1 row-span-3 overflow-hidden rounded-lg bg-blue-950 p-4 md:col-span-3 md:row-span-3 lg:col-span-3">
+          <div class="absolute top-15 left-0 z-0 h-full w-full bg-[url(/feature-3.png)] bg-center bg-no-repeat transition-all duration-700 ease-in-out group-hover:left-0 group-hover:group-hover:drop-shadow-[0_0_20px] group-hover:drop-shadow-blue-500 md:left-45"></div>
           <h3 class="subtitle z-10">Real-Time Sync Across Devices</h3>
         </div>
 

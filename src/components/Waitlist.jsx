ae55de0b2a3cd1ed1226f6a714ca913632a66113
techShabaka Agency <shahriarcode69@gmail.com>
2025-10-04 15:44:59 +0600

    wait list update

---
+++ src/components/Waitlist.jsx
@@ -0,0 +1,29 @@
+import { Badge } from "@/components/ui/badge";
+import { Input } from "@/components/ui/input";
+
+const Waitlist = () => {
+  return (
+    <section className="section mx relative container isolate rounded-2xl bg-blue-950 bg-[url(/waitlistbg.png)] bg-cover bg-right py-12">
+      <div class="absolute inset-0 -z-1 bg-black/30 backdrop-blur-xs"></div>
+      <div className="">
+        <div className="flex-center mb-12 flex-col gap-1 text-center">
+          <Badge variant="default">MileStack.</Badge>
+          <h2 className="heading-xl">Join The Waitlist Now</h2>
+          <p className="description text-center">
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam
+            nesciunt temporibus animi!
+          </p>
+        </div>
+
+        <form action="" className="flex-center flex-col gap-2 md:flex-row">
+          <Input placeholder="Enter your Email" />
+          <button className="primary-btn box-shadow" type="submit">
+            Join Waitlist
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+};
+
+export default Waitlist;
